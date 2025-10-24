a69dfa633c6cbf725ffcd1acff1f40b24ac17a25
Rona422 <etoumasato@gmail.com>
2025-10-24 15:30:07 +0900

    AllPushAndMatome

--- PartyProject/Assets/Scenes/GameScene/MiniGame/1v3/JumpingBar_1_v_33_WF/PlayerBase_JumpingBar_1v3.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlayerBase_JumpingBar_1v3 : PlayerBase
-{
-    public abstract void GameStart();
-    public abstract void GameUpdate();
-}
